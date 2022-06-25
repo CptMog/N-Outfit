3d68ad5f9d8c0e906075f277776e023ee6012015
CptMog <djawad.mohamed8@gmail.com>
2022-06-25 13:44:51 +0200

    Updating Filter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import Banner from './Banner.js';
+import Filter from './Filter.js';
 import './../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Filter />
     </div>
   );
 }
---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+import './../styles/Filter.css';
+
+export default function Filter(){
+ 
+    return(
+        <div className="container-filter">
+            <ul>
+                <li>Promos</li>
+                <li>TOPs</li>
+                <li>Shorts</li>
+                <li>lots</li>
+            </ul>
+        </div>
+    )
+
+}
